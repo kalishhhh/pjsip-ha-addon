1f2f3c31524ad91af356c0883cb44f2b67be8c4c
kalishhhh <kaliaishit@gmail.com>
2026-02-10 14:26:34 +0530

    Fix line endings for run.sh

--- pjsip-client/run.sh
+++ pjsip-client/run.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
 
-echo "Starting PJSIP Client..."
+CONFIG_DIR=/data
+ACCOUNT_FILE="$CONFIG_DIR/accounts"
 
-# Run SIP service in foreground
-exec python3 /app/sip_service.py
+SERVER=$(jq -r '.server_ip' /data/options.json)
+EXT=$(jq -r '.extension' /data/options.json)
+PASS=$(jq -r '.password' /data/options.json)
+
+mkdir -p "$CONFIG_DIR"
+
+echo "sip:$EXT@$SERVER;auth_user=$EXT;auth_pass=$PASS" > "$ACCOUNT_FILE"
+
+echo "Starting baresip..."
+exec baresip -f "$CONFIG_DIR"
